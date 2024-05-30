a00c83001a5e67a2a6b9111096d67e29a14bdcce
mrquangthai278 <mrquangthai278@gmail.com>
2024-05-30 15:30:10 +0700

    Update

--- src/components/combination/ColorItem.vue
+++ src/components/combination/ColorItem.vue
@@ -1,16 +1,9 @@
 <template>
-  <div
-    class="block relative group"
-    :class="customClass ? customClass : 'h-[75px] lg:h-[100px]'"
-    :style="getItemStyles"
-    @click="$emit('click', data)"
-  >
-    <div
-      v-if="canCopy || isMessaging"
+  <div class="block relative group" :class="customClass ? customClass : 'h-[75px] lg:h-[100px]'" :style="getItemStyles"
+    @click="$emit('click', data)">
+    <div v-if="canCopy || isMessaging"
       class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-10 cursor-pointer group-hover:flex w-full h-full items-center justify-center text-white"
-      :class="[isMessaging ? 'flex' : 'hidden']"
-      @click="handleClickCopy"
-    >
+      :class="[isMessaging ? 'flex' : 'hidden']" @click="handleClickCopy">
       <span v-if="!isCopied">Copy</span>
       <i v-else class="fa-solid fa-check"></i>
     </div>
@@ -59,6 +52,12 @@ export default {
   },
 
   methods: {
+    resetStateCopy() {
+      this.isMessaging = false;
+      this.isCopied = false;
+    },
+
+
     async handleClickCopy() {
       try {
         await navigator.clipboard.writeText(this.getDataValue);
@@ -66,14 +65,21 @@ export default {
         this.isMessaging = true;
 
         setTimeout(() => {
-          this.isMessaging = false;
-          this.isCopied = false;
+          this.resetStateCopy()
         }, 2000);
       } catch (err) {
         console.error("Failed to copy: ", err);
       }
     },
   },
+
+  watch: {
+    data: {
+      deep: true,
+      handler() {
+        this.resetStateCopy()
+      }
+    }
+  }
 };
 </script>
-    
